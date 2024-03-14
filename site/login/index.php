f72b6b0fe378fd37a6700677450b9a8231a736a3
Lorenzo Ianotto <lorenzus.ian8@gmail.com>
2024-03-14 11:38:04 +0100

    Move to BS 5

--- site/login/index.php
+++ site/login/index.php
@@ -22,22 +22,22 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         <?php require("../../components/bootstrap.php"); ?>
         <title>Accedi</title>
     </head>
-    <body class="container min-vh-100 justify-content-center d-flex flex-column">
+    <body class="container min-vh-100 justify-content-center align-items-center d-flex flex-column">
         <div class="card">
             <form class="card-body" method="post">
                 <h1 class="card-title">Accedi</h1>
-                <div class="form-group">
-                    <label for="email-input">Email:</label>
+                <div class="mb-2">
+                    <label class="form-label" for="email-input">Email</label>
                     <input class="form-control <?php if ($error === LoginError::NonExistantUser) echo "is-invalid" ?>" type="email" name="email" id="email-input" />
                     <span class="invalid-feedback">Utente non esistente</span>
                 </div>
-                <div class="form-group">
-                    <label for="password-input">Password:</label>
+                <div class="mb-4">
+                    <label class="form-label" for="password-input">Password</label>
                     <input class="form-control <?php if ($error === LoginError::WrongPassord) echo "is-invalid" ?> "type="password" name="password" id="password-input" />
                     <span class="invalid-feedback"">Password errata</span>
                 </div>
                 <button class="btn btn-primary" type="submit">Accedi</button>
-                <span class="ml-2">
+                <span class="ms-2 align-middle">
                     Non sei ancora iscritto? <a href="/site/register">Registrati</a>
                 </span>
             </form>
--- site/register/index.php
+++ site/register/index.php
@@ -40,48 +40,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
         <div class="card">
             <form class="card-body" method="post">
                 <h1 class="card-title">Nuovo utente</h1>
-                <div class="form-row">
-                    <div class="form-group col">
-                        <label for="email-input">Email</label>
+                <div class="row mb-2">
+                    <div class="col">
+                        <label class="form-label" for="email-input">Email</label>
                         <input class="form-control <?php if ($error === RegistrationError::AlreadyExistantUser) echo "is-invalid" ?>" type="email" name="email" id="email-input" required/>
                         <span class="invalid-feedback">Utente già esistente</span>
                     </div>
-                    <div class="form-group col">
-                        <label for="password-input">Password</label>
+                    <div class="col">
+                        <label class="form-label" for="password-input">Password</label>
                         <input class="form-control" type="password" name="password" id="password-input" required/>
                     </div>
                 </div>
-                <div class="form-row">
-                    <div class="form-group col">
-                        <label for="name-input">Nome</label>
+                <div class="row mb-2">
+                    <div class="col">
+                        <label class="form-label" for="name-input">Nome</label>
                         <input class="form-control" type="text" name="name" id="name-input" required/>
                     </div>
-                    <div class="form-group col">
-                        <label for="surname-input">Cognome</label>
+                    <div class="col">
+                        <label class="form-label" for="surname-input">Cognome</label>
                         <input class="form-control" type="text" name="surname" id="surname-input" required/>
                     </div>
                 </div>
-                <div class="form-row">
-                    <div class="form-group col">
-                        <label for="birth-date-input">Data di nascita</label>
+                <div class="row mb-4">
+                    <div class="col">
+                        <label class="form-label" for="birth-date-input">Data di nascita</label>
                         <input class="form-control" type="date" name="birth_date" id="birth-date-input" required/>
                     </div>
-                    <div class="form-group col">
-                        <label for="gender-input">Sesso</label>
-                        <select class="form-control" name="gender" id="gender-input" required>
+                    <div class="col">
+                        <label class="form-label" for="gender-input">Sesso</label>
+                        <select class="form-control form-select" name="gender" id="gender-input" required>
                             <option hidden disabled selected value></option>
                             <option value="male">Uomo</option>
                             <option value="female">Donna</option>
                             <option value="other">Australopiteco</option>
                         </select>
                     </div>
-                    <div class="form-group col">
-                        <label for="phone-number-input">Numero di telefono</label>
+                    <div class="col">
+                        <label class="form-label" for="phone-number-input">Numero di telefono</label>
                         <input class="form-control" type="tel" name="phone_number" id="phone-number-input" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" required/>
                     </div>
                 </div>
                 <button class="btn btn-primary" type="submit">Registrati</button>
-                <span class="ml-2">
+                <span class="ms-2 align-middle">
                     Sei già iscritto? <a href="/site/login">Accedi</a>
                 </span>
             </form>
