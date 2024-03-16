003bfe4dc270369a8bb9b3d86aaff3474ed44e1d
Lorenzo Ianotto <lorenzus.ian8@gmail.com>
2024-03-16 16:41:03 +0100

    Admin page

---
+++ site/admin/index.php
@@ -0,0 +1,57 @@
+<?php
+require_once "../../lib/auth.php";
+require_once "../../lib/db.php";
+session_start();
+protect_page(UserRole::Admin);
+
+$date_from = DateTime::createFromFormat("Y-m-d", $_GET["date_from"]) ? $_GET["date_from"] : "1970-01-01";
+$date_to = DateTime::createFromFormat("Y-m-d", $_GET["date_to"]) ? $_GET["date_to"] : "9999-12-31";
+
+$conn = DatabaseConnection::get_instance();
+$stmt = $conn->prepare("SELECT created_at AS Data, name AS Nome, surname AS Cognome, total_amount AS Totale, payment_method AS `Metodo di pagamento` FROM User
+    JOIN CustomerOrder ON User.id=CustomerOrder.customer_id
+    WHERE created_at BETWEEN ? AND ?
+    ORDER BY created_at ASC");
+$stmt->bind_param("ss", $date_from, $date_to);
+$stmt->execute();
+$res = $stmt->get_result();
+
+
+function sql_result_table(mysqli_result $res) {
+    echo "<table class=\"table\"><thead><tr>";
+
+    while ($field = $res->fetch_field()) {
+        echo "<th>".$field->name."</th>";
+    }
+
+    echo "</tr></thead><tbody>";
+
+    while ($row = $res->fetch_assoc()) {
+        echo "<tr>";
+        foreach ($row as $field) {
+            echo "<td>".$field."</td>";
+        }
+        echo "</tr>";
+    }
+
+    echo "</tbody></table>";
+}
+?>
+<html>
+    <head>
+        <?php require("../../components/bootstrap.php"); ?>
+        <title>Homepage Admin</title>
+    </head>
+    <body>
+        <main class="container">
+            <form>
+                <label for="date-from-input">Da</label>
+                <input type="date" name="date_from" id="date-from-input"/>
+                <label for="date-to-input">A</label>
+                <input type="date" name="date_to" id="date-to-input"/>
+                <button type="submit">Cerca</button>
+            </form>
+            <?php sql_result_table($res) ?>
+        </main>
+    </body>
+</html>
