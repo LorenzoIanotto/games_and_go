cd9494fe8c4b81a278f1d0bb2f7e4042b638d2c0
Lorenzo Ianotto <lorenzus.ian8@gmail.com>
2024-03-17 10:43:37 +0100

    get product quantity

---
+++ lib/db_actions/product.php
@@ -0,0 +1,17 @@
+<?php
+function get_product_quantity(int $product_id): int|null {
+
+    $conn = DatabaseConnection::get_instance();
+    $stmt = $conn->prepare("SELECT quantity FROM Product WHERE id=?");
+    $stmt->bind_param("i", $product_id);
+    $stmt->execute();
+    $res = $stmt->get_result();
+
+    if (!$res) return null;
+
+    $row = $res->fetch_assoc();
+
+    if (!$row) return null;
+
+    return intval($row["quantity"]);
+}
